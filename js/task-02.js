1cae4c22e3270888b54ce00e6734ee0a6fa82f3f
Dmytro Morozov <marikwork@gmail.com>
2023-01-29 15:20:09 -0700

    Update task-02.js
    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const menuList = document.querySelector("ul#ingredients");//знаходимо необхідний вузол, в цьому випадку список з id ingredients
+
+const ingredientsListMenu = [];                           // створюємо масив для зберігання нових елементів списку
+
+ingredients.forEach((ingredient) => {                     //циклом проходимося по всім елементам вхідного масиву, за яким будемо будувати новий список
+  let item = document.createElement("li");                // створюємо нову лішку
+  item.textContent = ingredient;                          // додаємо контент лішки
+  item.classList.add("item");                             // присвоюємо клас 
+  ingredientsListMenu.push(item);                         // додаємо елемент до нового масиву
+});
+menuList.append(...ingredientsListMenu);                  // створюємо всі лішки в розмітці  за одну операцію
+
+
+
