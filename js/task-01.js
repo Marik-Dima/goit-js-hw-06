a84e3518f6dfe47f74a59e909ebf32dc62c25e84
Dmytro Morozov <marikwork@gmail.com>
2023-01-29 20:47:52 -0700

    task-02 with map

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const listOfCategories = document.querySelector("#categories");
-const listOfClassItem = listOfCategories.querySelectorAll(".item");
-console.log(`Number of categories: ${listOfClassItem.length}`);
+const listOfCategories = document.querySelector("#categories");     // знаходимо список
+const listOfClassItem = listOfCategories.querySelectorAll(".item"); // обираємо всі елементи(лішки) батьківського списку тільки з класом item 
+console.log(`Number of categories: ${listOfClassItem.length}`);     // виводимо кількість елементів відповідаючіх умовам (клас item)
 
-listOfClassItem.forEach((item) => {
-    console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`)
+listOfClassItem.forEach((item) => {                                    // перебираємо 
+    console.log(`Category: ${item.firstElementChild.textContent}`);    // виводимо назву категоріі
+    console.log(`Elements: ${item.lastElementChild.children.length}`)  // виводимо кількість елементіи в категоріі
 });
--- js/task-02.js
+++ js/task-02.js
@@ -9,15 +9,24 @@ const ingredients = [
 
 const menuList = document.querySelector("ul#ingredients");//знаходимо необхідний вузол, в цьому випадку список з id ingredients
 
-const ingredientsListMenu = [];                           // створюємо масив для зберігання нових елементів списку
+// за допомогою forEach
+// const ingredientsListMenu = [];                           // створюємо масив для зберігання нових елементів списку
 
-ingredients.forEach((ingredient) => {                     //циклом проходимося по всім елементам вхідного масиву, за яким будемо будувати новий список
-  let item = document.createElement("li");                // створюємо нову лішку
-  item.textContent = ingredient;                          // додаємо контент лішки
-  item.classList.add("item");                             // присвоюємо клас 
-  ingredientsListMenu.push(item);                         // додаємо елемент до нового масиву
+// ingredients.forEach((ingredient) => {                     //циклом проходимося по всім елементам вхідного масиву, за яким будемо будувати новий список
+//   let item = document.createElement("li");                // створюємо нову лішку
+//   item.textContent = ingredient;                          // додаємо контент лішки
+//   item.classList.add("item");                             // присвоюємо клас 
+//   ingredientsListMenu.push(item);                         // додаємо елемент до нового масиву
+// });
+// menuList.append(...ingredientsListMenu);                  // створюємо всі лішки в розмітці  за одну операцію
+
+
+// за допомогою map
+const ingredientsListMenu = ingredients.map(ingredient => {
+  const item = document.createElement("li");                // створюємо нову лішку
+  item.textContent = ingredient;                            // додаємо контент лішки
+  item.classList.add("item");                               // присвоюємо клас 
+  return item;
 });
-menuList.append(...ingredientsListMenu);                  // створюємо всі лішки в розмітці  за одну операцію
 
-
-
+menuList.append(...ingredientsListMenu); 
