3e2f0f0cc0034e90fd424e7470c2060e3b4609c1
Dmytro Morozov <marikwork@gmail.com>
2023-02-06 09:18:33 -0700

    Task 08
    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
 
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener('submit', handleLoginFormSubmit);
+
+function handleLoginFormSubmit(event) { 
+    event.preventDefault();
+    const loginData = {
+        email : event.currentTarget.elements.email.value,
+        password : event.currentTarget.elements.password.value,
+    };
+
+    loginData.email === "" || loginData.password === ""
+        ? alert("Будь ласка, заповніть всі поля форми!")
+        : console.log(loginData);
+    
+    event.currentTarget.reset();
+};
