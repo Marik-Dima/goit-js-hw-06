c80c8337663865bd4936b8ec815a22e4b2eef8a6
Dmytro Morozov <marikwork@gmail.com>
2023-02-06 09:51:19 -0700

    task09
    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+
+const buttonChangeColor = document.querySelector(".change-color");
+const colorTextMessage = document.querySelector(".color");
+
+buttonChangeColor.addEventListener('click', handleChangeColorButtonClick);
+
+
+function handleChangeColorButtonClick() {
+  colorTextMessage.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorTextMessage.textContent;
+};
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
